2a02ea0b4776b2050f93675769d895af5acbdf3a
amy-phillips <amy@islovely.co.uk>
2017-01-21 15:55:06 +0000

    waves scene with paddle prompt

---
+++ Assets/WavesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavesScript : MonoBehaviour {
+
+ 
+    public float WAVE_VELOCITY = 2.0f;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        // move waves leftwards
+        transform.position = new Vector3(transform.position.x - Time.deltaTime * WAVE_VELOCITY, transform.position.y, transform.position.z);
+    }
+}
