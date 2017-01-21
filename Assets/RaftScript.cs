0878cb485e1d1ea629513534fee8b01ceeca29a3
amy-phillips <amy@islovely.co.uk>
2017-01-21 16:34:50 +0000

    detect button presses for paddling

--- Assets/RaftScript.cs
+++ Assets/RaftScript.cs
@@ -5,29 +5,69 @@ using UnityEngine;
 public class RaftScript : MonoBehaviour {
 
     public GameObject PaddlePrompt;
-    public WavesScript wavescript;
+    public GameObject PaddleSuccess;
+    public GameObject PaddleFail;
+    public WavesScript waveScript;
+    public int SuccessCount = 0;
+    public int FailCount = 0;
 
     float GetTimeTilWavePeak()
     {
-        float time_for_peak_to_pass = 1.15f * wavescript.WAVE_VELOCITY;
+        float time_for_peak_to_pass = 1.15f * waveScript.WAVE_VELOCITY;
         float time_since_peak = Time.time % time_for_peak_to_pass;
         return time_for_peak_to_pass- time_since_peak;
     }
 
-
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
+        SuccessCount = 0;
+        FailCount = 0;
+    }
+
+    float LastPaddleTime = -1.0f;
+    float LastPaddleSuccess = -1.0f;
+    float LastPaddleFail = -1.0f;
+
 	// Update is called once per frame
 	void Update () {
-		// display padd button prompt
-        if(GetTimeTilWavePeak() < 0.5f) {
+        // do we need to display paddle success/failure?
+        if (Time.time - LastPaddleSuccess < 0.5f) {
+            PaddleSuccess.SetActive(true);
+            PaddleFail.SetActive(false);
+            PaddlePrompt.SetActive(false);
+            return;
+        } else if (Time.time - LastPaddleFail < 0.5f) {
+            PaddleSuccess.SetActive(false);
+            PaddleFail.SetActive(true);
+            PaddlePrompt.SetActive(false);
+            return;
+        }
+
+        PaddleSuccess.SetActive(false);
+        PaddleFail.SetActive(false);
+
+        float time_til_wave_peak = GetTimeTilWavePeak();
+
+		// display paddle button prompt
+        if (GetTimeTilWavePeak() < 0.5f) {
             PaddlePrompt.SetActive(true);
         } else {
             PaddlePrompt.SetActive(false);
         }
 
-	}
+        // did we paddle?
+        if(Input.anyKeyDown) {
+            LastPaddleTime = Time.time;
+
+            // successfully?
+            if(time_til_wave_peak < 0.5f) {
+                LastPaddleSuccess = Time.time;
+                SuccessCount++;
+            } else {
+                LastPaddleFail = Time.time;
+                FailCount++;
+            }
+        }
+
+    }
 }
