3482093ca9e3fa968a54a71718f2db39c7a4b030
amy-phillips <amy@islovely.co.uk>
2017-01-22 10:34:47 +0000

    added settings to gamemanager for text speed

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    public const float DEFAULT_TEXT_SPEED = 1.0f;
+    public float PlayerTextSpeed = DEFAULT_TEXT_SPEED;
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public Slider TextSpeedSlider;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerTextSpeed = PlayerPrefs.GetFloat("PlayerTextSpeed", DEFAULT_TEXT_SPEED);
+        TextSpeedSlider.value = PlayerTextSpeed;
+        ShowMainMenu();
+    }
 
     void Awake()
     {
@@ -16,4 +30,26 @@ public class GameManager : MonoBehaviour {
         //maybe queue this somewhere?
         SceneManager.LoadSceneAsync(sceneName);
     }
+
+    public void ShowMainMenu()
+    {
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void ShowSettings()
+    {
+        TextSpeedSlider.value = PlayerTextSpeed;
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerTextSpeed = TextSpeedSlider.value;
+        PlayerPrefs.SetFloat("PlayerTextSpeed", PlayerTextSpeed);
+        ShowMainMenu();
+    }
+
+    
 }
