5590c105486c5ebb60399f5a04549f140d5ac0e9
Cherie Davidson <cherie.simone@gmail.com>
2017-01-22 11:49:41 +0000

    Moved story object to the game manager and made it persistent, setup interaction between scenes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Ink.Runtime;
 
 public class GameManager : MonoBehaviour {
+
+    public static GameManager inst;
+
+    [SerializeField]
+    private TextAsset storyJSON;
 
     public const float DEFAULT_TEXT_SPEED = 1.0f;
     public float PlayerTextSpeed = DEFAULT_TEXT_SPEED;
     public GameObject MainMenu;
     public GameObject SettingsMenu;
     public Slider TextSpeedSlider;
+
+    public bool waveSectionComplete = false;
+
+    public float difficulty;
+    public bool paddlingSuccess;
+    public Story story;
+
+
+    void Awake()
+    {
+        if (inst != null)
+        {
+            Destroy(this);
+        }
+        inst = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Use this for initialization
     void Start()
@@ -20,16 +43,33 @@ public class GameManager : MonoBehaviour {
         ShowMainMenu();
     }
 
-    void Awake()
+    //GAME MANAGEMENT
+
+    public void StartGame()
     {
+        story = new Story(storyJSON.text);
+        waveSectionComplete = false;
+        paddlingSuccess = false;
+        SceneManager.LoadScene("Dialogue");
 
     }
 
-	public void LoadGame(string sceneName)
+    public void LoadWaveSection(float d = 0)
     {
-        //maybe queue this somewhere?
-        SceneManager.LoadSceneAsync(sceneName);
+        waveSectionComplete = false;
+        difficulty = d;
+        SceneManager.LoadScene("Waves");   
     }
+
+    public void CompleteWaveSection(bool success)
+    {
+        waveSectionComplete = true;
+        paddlingSuccess = success;
+        SceneManager.LoadScene("Dialogue"); 
+    }
+
+
+    //SETTINGS INFO
 
     public void ShowMainMenu()
     {
--- Assets/InkScripts/gamescript.ink
+++ Assets/InkScripts/gamescript.ink
@@ -3,7 +3,7 @@ VAR rightslot = "empty"
 VAR elias = 1
 VAR textspeed = 1
 
-VAR paddlingsection = false
+VAR paddlingsection = "false"
 
 VAR rowland = "alive"
 VAR cheddar = "alive"
@@ -94,7 +94,7 @@ Failure can have unexpected consequences.
 
 === chapter2 ===
 = initialsetup
-~ paddlingsection = true 
+~ paddlingsection = "true"
 *[SUCCEED] ->success
 *[FAIL] -> fail
 
